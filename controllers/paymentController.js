5a09792622f9adcd4a6f57fcf0ed9375b235a028
DungLQ <dunglq@niw.com.vn>
2023-02-16 16:33:14 +0700

    fix Library

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,22 +1,22 @@
-import { catchAsyncError } from '../middlewares/catchAsyncError.js';
-import { User } from '../models/User.js';
-import ErrorHandler from '../utils/errorHandler.js';
-import { instance } from '../server.js';
-import crypto from 'crypto';
-import { Payment } from '../models/Payment.js';
-import * as Message from '../constants/Message.js';
-import { v4 as uuidv4 } from 'uuid';
-import https from 'https';
-import dateFormat from 'dateformat';
-import querystring from 'qs';
+import { catchAsyncError } from "../middlewares/catchAsyncError.js";
+import { User } from "../models/User.js";
+import ErrorHandler from "../utils/errorHandler.js";
+import { instance } from "../server.js";
+import crypto from "crypto";
+import { Payment } from "../models/Payment.js";
+import * as Message from "../constants/Message.js";
+import { v4 as uuidv4 } from "uuid";
+import https from "https";
+import dateFormat from "dateformat";
+import querystring from "qs";
 
 export const buySubscription = catchAsyncError(async (req, res, next) => {
   const user = await User.findById(req.user._id);
 
-  if (user.role === 'admin')
+  if (user.role === "admin")
     return next(new ErrorHandler("Admin can't buy subscription", 400));
 
-  const plan_id = process.env.PLAN_ID || 'plan_JuJevKAcuZdtRO';
+  const plan_id = process.env.PLAN_ID || "plan_JuJevKAcuZdtRO";
 
   const subscription = await instance.subscriptions.create({
     plan_id,
@@ -45,9 +45,9 @@ export const paymentVerification = catchAsyncError(async (req, res, next) => {
   const subscription_id = user.subscription.id;
 
   const generated_signature = crypto
-    .createHmac('sha256', process.env.RAZORPAY_API_SECRET)
-    .update(razorpay_payment_id + '|' + subscription_id, 'utf-8')
-    .digest('hex');
+    .createHmac("sha256", process.env.RAZORPAY_API_SECRET)
+    .update(razorpay_payment_id + "|" + subscription_id, "utf-8")
+    .digest("hex");
 
   const isAuthentic = generated_signature === razorpay_signature;
 
@@ -61,7 +61,7 @@ export const paymentVerification = catchAsyncError(async (req, res, next) => {
     razorpay_subscription_id,
   });
 
-  user.subscription.status = 'active';
+  user.subscription.status = "active";
 
   await user.save();
 
@@ -106,18 +106,18 @@ export const cancelSubscription = catchAsyncError(async (req, res, next) => {
   res.status(200).json({
     success: true,
     message: refund
-      ? 'Subscription cancelled, You will receive full refund within 7 days.'
-      : 'Subscription cancelled, Now refund initiated as subscription was cancelled after 7 days.',
+      ? "Subscription cancelled, You will receive full refund within 7 days."
+      : "Subscription cancelled, Now refund initiated as subscription was cancelled after 7 days.",
   });
 });
 
 export const getPaymentByUser = async (req, res) => {
   try {
-    const { page = 1, limit = 10, search = '', field = 'name' } = req.query;
+    const { page = 1, limit = 10, search = "", field = "name" } = req.query;
     const paymentInformation = await User.find(
       {
         [field]: new RegExp(search),
-        role: 'user',
+        role: "user",
       },
       {
         name: 1,
@@ -128,7 +128,7 @@ export const getPaymentByUser = async (req, res) => {
         limit: limit,
         skip: (page - 1) * limit,
       }
-    ).sort('name');
+    ).sort("name");
 
     res.status(200).json({
       success: true,
@@ -149,7 +149,7 @@ export const activate = async (req, res) => {
   try {
     const { userId } = req.params;
     const user = await User.findById(userId);
-    user.subscription.status = 'active';
+    user.subscription.status = "active";
     user.subscription.id = uuidv4();
     await user.save();
 
@@ -169,7 +169,7 @@ export const deactivate = async (req, res) => {
   try {
     const { userId } = req.params;
     const user = await User.findById(userId);
-    user.subscription.status = 'created';
+    user.subscription.status = "created";
     await user.save();
 
     res.status(200).json({
@@ -187,52 +187,52 @@ export const deactivate = async (req, res) => {
 export const sendmomo = catchAsyncError(async (req, res, next) => {
   const user = await User.findById(req.params.userId);
   if (!user) {
-    return next(new ErrorHandler('User not Found'), 404);
+    return next(new ErrorHandler("User not Found"), 404);
   }
-  if (user.subscription && user.subscription.status === 'active') {
-    return next(new ErrorHandler('User has been paid', 403));
+  if (user.subscription && user.subscription.status === "active") {
+    return next(new ErrorHandler("User has been paid", 403));
   }
 
-  var partnerCode = 'MOMO';
+  var partnerCode = "MOMO";
   var accessKey = process.env.MOMO_ACCESSKEY;
   var secretkey = process.env.MOMO_SECRETKEY;
   var requestId = partnerCode + new Date().getTime();
   var orderId = requestId;
-  var orderInfo = 'Register to be Premium User !'; // name: ten hoa don
+  var orderInfo = "Register to be Premium User !"; // name: ten hoa don
   var redirectUrl = `${process.env.URL_WEBSITE}/api/v1/pay/momo/success`;
   var ipnUrl = `${process.env.URL_WEBSITE}/api/v1/pay/momo/success`;
   // var ipnUrl = redirectUrl = "https://webhook.site/454e7b77-f177-4ece-8236-ddf1c26ba7f8";
   var amount = 299 * 23;
-  var requestType = 'captureWallet';
+  var requestType = "captureWallet";
   var extraData = req.params.userId; //pass empty value if your merchant does not have stores
   //before sign HMAC SHA256 with format
   //accessKey=$accessKey&amount=$amount&extraData=$extraData&ipnUrl=$ipnUrl&orderId=$orderId&orderInfo=$orderInfo&partnerCode=$partnerCode&redirectUrl=$redirectUrl&requestId=$requestId&requestType=$requestType
   var rawSignature =
-    'accessKey=' +
+    "accessKey=" +
     accessKey +
-    '&amount=' +
+    "&amount=" +
     amount +
-    '&extraData=' +
+    "&extraData=" +
     extraData +
-    '&ipnUrl=' +
+    "&ipnUrl=" +
     ipnUrl +
-    '&orderId=' +
+    "&orderId=" +
     orderId +
-    '&orderInfo=' +
+    "&orderInfo=" +
     orderInfo +
-    '&partnerCode=' +
+    "&partnerCode=" +
     partnerCode +
-    '&redirectUrl=' +
+    "&redirectUrl=" +
     redirectUrl +
-    '&requestId=' +
+    "&requestId=" +
     requestId +
-    '&requestType=' +
+    "&requestType=" +
     requestType;
 
   var signature = crypto
-    .createHmac('sha256', secretkey)
+    .createHmac("sha256", secretkey)
     .update(rawSignature)
-    .digest('hex');
+    .digest("hex");
 
   const requestBody = JSON.stringify({
     partnerCode: partnerCode,
@@ -246,41 +246,41 @@ export const sendmomo = catchAsyncError(async (req, res, next) => {
     extraData: extraData,
     requestType: requestType,
     signature: signature,
-    lang: 'en',
+    lang: "en",
   });
 
   const options = {
-    hostname: 'test-payment.momo.vn',
+    hostname: "test-payment.momo.vn",
     port: 443,
-    path: '/v2/gateway/api/create',
-    method: 'POST',
+    path: "/v2/gateway/api/create",
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': Buffer.byteLength(requestBody),
+      "Content-Type": "application/json",
+      "Content-Length": Buffer.byteLength(requestBody),
     },
   };
 
   const momoRequest = (options, requestBody, res) => {
-    let bodyRequest = '';
+    let bodyRequest = "";
     const request = https.request(options, (response) => {
       console.log(`Status: ${response.statusCode}`);
       console.log(`Headers: ${JSON.stringify(response.headers)}`);
-      response.setEncoding('utf8');
-      response.on('data', (body) => {
+      response.setEncoding("utf8");
+      response.on("data", (body) => {
         bodyRequest += body;
       });
       console.log(bodyRequest);
-      response.on('end', () => {
-        console.log('No more data in response.');
+      response.on("end", () => {
+        console.log("No more data in response.");
         console.log(JSON.parse(bodyRequest).payUrl);
         res.redirect(JSON.parse(bodyRequest).payUrl);
       });
     });
-    request.on('error', (e) => {
+    request.on("error", (e) => {
       console.log(`problem with request: ${e.message}`);
     });
     // write data to request body
-    console.log('Sending....');
+    console.log("Sending....");
     request.write(requestBody);
     request.end();
   };
@@ -294,21 +294,21 @@ export const momoSuccess = catchAsyncError(async (req, res, next) => {
   await User.findByIdAndUpdate(extraData, {
     subscription: {
       id: uuidv4().toString(),
-      status: 'active',
+      status: "active",
     },
   });
 
-  res.redirect('http://localhost:3000/courses');
+  res.redirect("http://localhost:3000/courses");
 });
 
 export const sendVNPay = catchAsyncError(async (req, res, next) => {
   const user = await User.findById(req.params.userId);
   try {
     if (!user) {
-      return next(new ErrorHandler('User not Found'), 404);
+      return next(new ErrorHandler("User not Found"), 404);
     }
-    if (user.subscription.status === 'active') {
-      return next(new ErrorHandler('User has been paid', 403));
+    if (user.subscription.status === "active") {
+      return next(new ErrorHandler("User has been paid", 403));
     }
   } catch (e) {
     return next(new ErrorHandler(e, 400));
@@ -325,13 +325,13 @@ export const sendVNPay = catchAsyncError(async (req, res, next) => {
     }
     str.sort();
     for (key = 0; key < str.length; key++) {
-      sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, '+');
+      sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
     }
     return sorted;
   };
 
   var ipAddr =
-    req.headers['x-forwarded-for'] ||
+    req.headers["x-forwarded-for"] ||
     req.connection.remoteAddress ||
     req.socket.remoteAddress ||
     req.connection.socket.remoteAddress;
@@ -343,49 +343,49 @@ export const sendVNPay = catchAsyncError(async (req, res, next) => {
 
   var date = new Date();
 
-  var createDate = dateFormat(date, 'yyyymmddHHMMss');
-  var orderId = dateFormat(date, 'HHmmss');
+  var createDate = dateFormat(date, "yyyymmddHHMMss");
+  var orderId = dateFormat(date, "HHmmss");
   var amount = 299 * 23000;
-  var bankCode = '';
+  var bankCode = "";
 
-  var orderInfo = 'payment for premium';
-  var orderType = 'billpayment';
-  var locale = 'vn';
-  if (locale === null || locale === '') {
-    locale = 'vn';
+  var orderInfo = "payment for premium";
+  var orderType = "billpayment";
+  var locale = "vn";
+  if (locale === null || locale === "") {
+    locale = "vn";
   }
-  var currCode = 'VND';
+  var currCode = "VND";
   var vnp_Params = {};
 
-  vnp_Params['vnp_Version'] = '2.1.0';
-  vnp_Params['vnp_Command'] = 'pay';
-  vnp_Params['vnp_TmnCode'] = tmnCode;
+  vnp_Params["vnp_Version"] = "2.1.0";
+  vnp_Params["vnp_Command"] = "pay";
+  vnp_Params["vnp_TmnCode"] = tmnCode;
   // vnp_Params["vnp_Merchant"] = req.body.orderId;
-  vnp_Params['vnp_Locale'] = locale;
-  vnp_Params['vnp_CurrCode'] = currCode;
-  vnp_Params['vnp_TxnRef'] = orderId;
-  vnp_Params['vnp_OrderInfo'] = orderInfo + ',' + user._id;
-  vnp_Params['vnp_OrderType'] = orderType;
-  vnp_Params['vnp_Amount'] = amount * 100;
-  vnp_Params['vnp_ReturnUrl'] = returnUrl;
-  vnp_Params['vnp_IpAddr'] = ipAddr;
-  vnp_Params['vnp_CreateDate'] = createDate;
+  vnp_Params["vnp_Locale"] = locale;
+  vnp_Params["vnp_CurrCode"] = currCode;
+  vnp_Params["vnp_TxnRef"] = orderId;
+  vnp_Params["vnp_OrderInfo"] = orderInfo + "," + user._id;
+  vnp_Params["vnp_OrderType"] = orderType;
+  vnp_Params["vnp_Amount"] = amount * 100;
+  vnp_Params["vnp_ReturnUrl"] = returnUrl;
+  vnp_Params["vnp_IpAddr"] = ipAddr;
+  vnp_Params["vnp_CreateDate"] = createDate;
   // vnp_Params["vnp_idOrder"] = req.body.orderId;
-  if (bankCode !== null && bankCode !== '') {
-    vnp_Params['vnp_BankCode'] = bankCode;
+  if (bankCode !== null && bankCode !== "") {
+    vnp_Params["vnp_BankCode"] = bankCode;
   }
 
   vnp_Params = sortObject(vnp_Params);
 
   var signData = querystring.stringify(vnp_Params, { encode: false });
 
-  var hmac = crypto.createHmac('sha512', secretKey);
+  var hmac = crypto.createHmac("sha512", secretKey);
 
-  var signed = hmac.update(Buffer.from(signData, 'utf-8')).digest('hex');
+  var signed = hmac.update(Buffer.from(signData, "utf-8")).digest("hex");
 
-  vnp_Params['vnp_SecureHash'] = signed;
+  vnp_Params["vnp_SecureHash"] = signed;
 
-  vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false });
+  vnpUrl += "?" + querystring.stringify(vnp_Params, { encode: false });
   console.log(vnpUrl);
   res.redirect(vnpUrl);
 });
@@ -402,16 +402,16 @@ export const VNPAYsuccess = catchAsyncError(async (req, res, next) => {
     }
     str.sort();
     for (key = 0; key < str.length; key++) {
-      sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, '+');
+      sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
     }
     return sorted;
   };
   var vnp_Params = req.query;
 
-  var secureHash = vnp_Params['vnp_SecureHash'];
+  var secureHash = vnp_Params["vnp_SecureHash"];
 
-  delete vnp_Params['vnp_SecureHash'];
-  delete vnp_Params['vnp_SecureHashType'];
+  delete vnp_Params["vnp_SecureHash"];
+  delete vnp_Params["vnp_SecureHashType"];
 
   vnp_Params = sortObject(vnp_Params);
 
@@ -419,20 +419,20 @@ export const VNPAYsuccess = catchAsyncError(async (req, res, next) => {
   var secretKey = process.env.VNP_HASHSECRET;
 
   var signData = querystring.stringify(vnp_Params, { encode: false });
-  var hmac = crypto.createHmac('sha512', secretKey);
-  var signed = hmac.update(Buffer.from(signData, 'utf-8')).digest('hex');
+  var hmac = crypto.createHmac("sha512", secretKey);
+  var signed = hmac.update(Buffer.from(signData, "utf-8")).digest("hex");
 
   if (secureHash === signed) {
     //Kiem tra xem du lieu trong db co hop le hay khong va thong bao ket qua
-    const userId = req.query.vnp_OrderInfo.split(',')[1];
+    const userId = req.query.vnp_OrderInfo.split(",")[1];
     await User.findByIdAndUpdate(userId, {
       subscription: {
         id: uuidv4().toString(),
-        status: 'active',
+        status: "active",
       },
     });
     // res.render("success", { code: vnp_Params["vnp_ResponseCode"] });
-    res.redirect('http://localhost:3000/courses');
+    res.redirect("http://localhost:3000/courses");
     // res.json({
     //    success: "success",
     //    code: { code: vnp_Params["vnp_ResponseCode"] },
@@ -440,6 +440,6 @@ export const VNPAYsuccess = catchAsyncError(async (req, res, next) => {
     //    order,
     // });
   } else {
-    res.render('success', { code: '97' });
+    res.render("success", { code: "97" });
   }
 });
--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 import {
   buySubscription,
   cancelSubscription,
@@ -11,30 +11,30 @@ import {
   momoSuccess,
   VNPAYsuccess,
   sendVNPay,
-} from '../controllers/paymentController.js';
-import { isAuthenticated } from '../middlewares/auth.js';
+} from "../controllers/paymentController.js";
+import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 // Buy Subscription
-router.route('/subscribe').get(isAuthenticated, buySubscription);
+router.route("/subscribe").get(isAuthenticated, buySubscription);
 // router.route('/pay/momo/:userId').get(isAuthenticated, sendmomo);
-router.route('/pay/momo/success').get(momoSuccess);
-router.route('/pay/momo/:userId').get(sendmomo);
+router.route("/pay/momo/success").get(momoSuccess);
+router.route("/pay/momo/:userId").get(sendmomo);
 
-router.route('/pay/vnpay/success').get(VNPAYsuccess);
-router.route('/pay/vnpay/:userId').get(sendVNPay);
+router.route("/pay/vnpay/success").get(VNPAYsuccess);
+router.route("/pay/vnpay/:userId").get(sendVNPay);
 
 // Verify Payment and save reference in database
-router.route('/paymentverification').post(isAuthenticated, paymentVerification);
+router.route("/paymentverification").post(isAuthenticated, paymentVerification);
 
 // Get Razorpay key
-router.route('/razorpaykey').get(getRazorPayKey);
+router.route("/razorpaykey").get(getRazorPayKey);
 
 // Cancel Subscription
-router.route('/subscribe/cancel').delete(isAuthenticated, cancelSubscription);
-router.route('/subscribe/info').get(getPaymentByUser);
-router.route('/subscribe/info/:userId').put(activate);
-router.route('/subscribe/info/:userId').delete(deactivate);
+router.route("/subscribe/cancel").delete(isAuthenticated, cancelSubscription);
+router.route("/subscribe/info").get(getPaymentByUser);
+router.route("/subscribe/info/:userId").put(activate);
+router.route("/subscribe/info/:userId").delete(deactivate);
 
 export default router;
